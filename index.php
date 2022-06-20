635847010f295ba6e43ac4818dbc8380c1b9d7b1
benjom22 <33999334+benjom22@users.noreply.github.com>
2022-06-20 12:37:41 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
- require 'vendor/autoload.php';
+$servername = "localhost";
+$username = "root";
+$password = "ha10be14";
+$schema="onlinemovirereservation"
 
- Flight::route('/', function(){
-   echo 'Hello world!';
- });
-
- Flight::start();
-
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 ?>
